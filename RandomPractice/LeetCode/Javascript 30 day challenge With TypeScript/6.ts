210531f06dbfbc1f1ccd21a1e994c977784a5f40
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-11-10 19:43:44 +0600

    Mongodb Practice

---
+++ RandomPractice/LeetCode/Javascript 30 day challenge With TypeScript/6.ts
@@ -0,0 +1,9 @@
+// type Fn = (accum: number, curr: number) => number;
+
+// function reduce(nums: number[], fn: Fn, init: number): number {
+//   let res: number = init;
+
+//   nums.forEach((n: number) => (res = fn(res, n)));
+
+//   return res;
+// }
---
+++ RandomPractice/LeetCode/Javascript 30 day challenge With TypeScript/7.ts
@@ -0,0 +1,15 @@
+// type F = (x: number) => number;
+
+// function compose(functions: F[]): F {
+//   return function (x) {
+//     for (let i = 0; i < functions.length; i++) {
+//       x = functions[functions.length - i - 1](x);
+//     }
+//     return x;
+//   };
+// }
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
