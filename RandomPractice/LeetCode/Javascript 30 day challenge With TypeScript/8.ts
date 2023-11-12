671fc541bc92ec69da56e5be4aabc36eeda2e478
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-11-12 19:02:59 +0600

    updated

---
+++ RandomPractice/LeetCode/Javascript 30 day challenge With TypeScript/8.ts
@@ -0,0 +1,28 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined;
+
+function once(fn: Function): OnceFn {
+  let called: boolean = false;
+  return function (...args: any[]) {
+    if (called) {
+      return undefined;
+    }
+    called = true;
+    // return fn(...args);
+    return fn.apply(this, args);
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
---
+++ RandomPractice/LeetCode/Javascript 30 day challenge With TypeScript/9.ts
@@ -0,0 +1,13 @@
+type Fn = (...params: number[]) => number;
+
+function memoize(fn: Fn): Fn {
+  const cache = {};
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    cache[key] = fn(...args);
+    return cache[key];
+  };
+}
