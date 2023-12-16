1d7ebc0cb2a85c074a54cca20cea6d25129fb3a8
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-16 10:54:59 +0600

    Keys, prop types in React

--- Z-Practice/first-react-app/src/App.jsx
+++ Z-Practice/first-react-app/src/App.jsx
@@ -1,11 +1,32 @@
-import Slots from "./Slots";
-import "./App.css";
+import Slots from './Slots';
+import './App.css';
+import ShoppingList from './ShoppingList';
+import PropertyList from './PropertyList';
+
+// const data = [
+//   { id: 1, item: 'eggs', quantity: 12, completed: false },
+//   { id: 2, item: 'milk', quantity: 1, completed: true },
+//   { id: 3, item: 'chicken breasts', quantity: 4, completed: false },
+//   { id: 4, item: 'carrots', quantity: 6, completed: true },
+//   { id: 5, item: 'apple', quantity: 8, completed: false },
+// ];
+
+const properties = [
+  { id: 129031, name: 'Desert Yurt', rating: 4.9, price: 150 },
+  { id: 129331, name: 'Lone Mountain Cabin', rating: 4.8, price: 250 },
+  { id: 129032, name: 'Cactus Retreat', rating: 4.75, price: 300 },
+  { id: 129033, name: 'Redwood Treehopper Escape', rating: 4.9, price: 120 },
+  { id: 129034, name: 'Oceanic Condo', rating: 4.7, price: 140 },
+  { id: 129035, name: 'Gold Miner Campground', rating: 4.69, price: 96 },
+];
 
 function App() {
   return (
     <div>
-      <Slots val1="🍌" val2="🍌" val3="🍌" />
-      <Slots val1="🍌" val2="🍌f" val3="🍌" />
+      <PropertyList properties={properties}></PropertyList>
+      {/* <ShoppingList items={data}></ShoppingList> */}
+      {/* <Slots val1="🍌" val2="🍌" val3="🍌" />
+      <Slots val1="🍌" val2="🍌f" val3="🍌" /> */}
     </div>
   );
 }
---
+++ Z-Practice/first-react-app/src/ShoppingListItem.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+export default function ShoppingListItem({ item, quantity, completed }) {
+  const styles = {
+    color: completed ? 'gray' : 'red',
+    textDecoration: completed ? 'line-through' : 'none',
+  };
+  return (
+    <li style={styles}>
+      {item} - {quantity}
+    </li>
+  );
+}
+
+ShoppingListItem.propTypes = {
+  item: PropTypes.string,
+  quantity: PropTypes.number,
+  completed: PropTypes.bool,
+};
