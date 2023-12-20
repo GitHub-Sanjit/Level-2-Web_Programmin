3faeaf04123e0ad39802b7a3557b12c3eb517122
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-12-20 21:17:22 +0600

    Effects in React

---
+++ Z-Practice/effects/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import ProfileViewerWithSearch from './ProfileViewerWithSearch';
+import QuoteFetcher from './QuoteFetcher';
+import QuoteFetcherLoader from './QuoteFetcherLoader';
+import Counter from './counter';
+
+function App() {
+  return (
+    <>
+      {/* <Counter></Counter> */}
+      {/* <QuoteFetcher></QuoteFetcher> */}
+      {/* <QuoteFetcherLoader></QuoteFetcherLoader> */}
+      <ProfileViewerWithSearch></ProfileViewerWithSearch>
+    </>
+  );
+}
+
+export default App;
